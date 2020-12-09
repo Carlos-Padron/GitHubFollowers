f5d999642591af209143d3bfe68e260252b22ca7
Carlos Padron <c_0509@hotmail.com>
2020-12-09 01:33:12 -0600

    follower and user models

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Carlos on 09/12/20.
+//
+
+import Foundation
+
+struct Follower: Codable{
+    var login: String
+    var avatarUrl: String
+}
