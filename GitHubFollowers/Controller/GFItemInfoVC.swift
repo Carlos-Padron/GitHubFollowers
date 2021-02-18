c2c45073e34be4ed744e499e6c023d5247da4ba1
Carlos Padron <c_0509@hotmail.com>
2021-02-17 21:39:39 -0600

    itemInfoVC super class was created

---
+++ GitHubFollowers/Controller/GFItemInfoVC.swift
@@ -0,0 +1,60 @@
+//
+//  GFItemInfoVC.swift
+//  GitHubFollowers
+//
+//  Created by Carlos on 17/02/21.
+//
+
+import UIKit
+
+class GFItemInfoVC: UIViewController {
+
+    let stackView        = UIStackView()
+    let itemInfoViewOne  = GFItemInfoView()
+    let itemInfoViewTwo  = GFItemInfoView()
+    let actionButton     = GFButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureBackgroundView()
+        layOutUI()
+        configureStackView()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func configureBackgroundView(){
+        view.backgroundColor     = .secondarySystemBackground
+        view.layer.cornerRadius  = 18
+    }
+    
+    private func layOutUI(){
+        let padding: CGFloat =  20
+        
+        view.addSubview(stackView)
+        view.addSubview(actionButton)
+        
+        stackView.translatesAutoresizingMaskIntoConstraints    = false
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: view.topAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            stackView.leadingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            stackView.heightAnchor.constraint(equalToConstant: 50),
+            
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
+            actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+            actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            actionButton.heightAnchor.constraint(equalTo: view.heightAnchor, constant: 44),
+            
+        ])
+    }
+    
+    private func configureStackView(){
+        stackView.axis          = .horizontal
+        stackView.distribution  = .equalSpacing
+        
+        stackView.addArrangedSubview(itemInfoViewOne)
+        stackView.addArrangedSubview(itemInfoViewTwo)
+    }
+
+}
--- GitHubFollowers/View/Views/GFItemInfoView.swift
+++ GitHubFollowers/View/Views/GFItemInfoView.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum ItemInfoType {
+    case repo, gist, followers, following
+}
+
 class GFItemInfoView: UIView {
 
     let symbolImageView  = UIImageView()
@@ -15,10 +19,61 @@ class GFItemInfoView: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func configure(){
+        addSubview(symbolImageView)
+        addSubview(titleLabel)
+        addSubview(countLabel)
+        
+        symbolImageView.translatesAutoresizingMaskIntoConstraints = false
+        symbolImageView.contentMode = .scaleAspectFill
+        symbolImageView.tintColor   = .label
+        
+        NSLayoutConstraint.activate([
+            symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
+            symbolImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            symbolImageView.heightAnchor.constraint(equalToConstant: 20),
+            symbolImageView.widthAnchor.constraint(equalToConstant: 20),
+            
+            titleLabel.centerYAnchor.constraint(equalTo: symbolImageView.centerYAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: symbolImageView.trailingAnchor, constant: 12),
+            titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            titleLabel.heightAnchor.constraint(equalToConstant: 18),
+            
+            countLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 4),
+            countLabel.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            countLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            countLabel.heightAnchor.constraint(equalToConstant: 18)
+        ])
+    }
+    
+    func set(itemInfoType: ItemInfoType, withCount count: Int){
+        switch itemInfoType {
+        case .repo:
+            symbolImageView.image  = UIImage(systemName: SFSymbols.folder)
+            titleLabel.text = "Public Repos"
+            break
+        case .gist:
+            symbolImageView.image  = UIImage(systemName: SFSymbols.gist)
+            titleLabel.text = "Public Gists"
+            break
+        case .followers:
+            symbolImageView.image  = UIImage(systemName: SFSymbols.followers)
+            titleLabel.text = "Followers"
+            break
+        case .following:
+            symbolImageView.image  = UIImage(systemName: SFSymbols.following)
+            titleLabel.text = "Following"
+            break
+        }
+        countLabel.text = String(count)
     }
     
 }
